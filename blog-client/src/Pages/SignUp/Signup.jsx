c56a34e19a7aa2b5a8f3cc6f4ef9c64df25a1ea7
prouzzal <prouzzal297@gmail.com>
2024-03-26 19:28:55 +0600

    Signup Page Ui design done

--- blog-client/src/Pages/SignUp/Signup.jsx
+++ blog-client/src/Pages/SignUp/Signup.jsx
@@ -1,7 +1,38 @@
-
+import { Button } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
     return (
-        <div>Signup</div>
+        <div className="min-h-screen mt-24">
+            <div>
+
+                <h2 className="text-4xl text-center font-semibold mb-14">Sign up with email</h2>
+                <form className="w-1/3 mx-auto">
+                    <div className="mb-5">
+                        <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your UserName</label>
+                        <input type="text" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your username" required />
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                        <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email address" required />
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter password</label>
+                        <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="password" />
+                    </div>
+                    <div className="mb-5">
+                        <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter confirm password</label>
+                        <input type="password" id="confirm-password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required placeholder="confirm password" />
+                    </div>
+
+                    <Button type="submit" className="text-white w-full" gradientDuoTone="purpleToBlue">Submit</Button>
+                </form>
+                <div className="flex gap-2 text-sm mt-5 items-center justify-center">
+                    <span>Have an account?</span>
+                    <Link to="/sign-in" className="text-blue-500">SIgn In</Link>
+
+                </div>
+            </div>
+        </div>
     )
 }

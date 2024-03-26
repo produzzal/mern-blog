39ecf7c749795104d2723a39983b27e299758e59
prouzzal <prouzzal297@gmail.com>
2024-03-26 17:43:50 +0600

    created signup api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import User from './models/user.model.js'
 
 dotenv.config();
 
@@ -10,5 +11,19 @@ mongoose.connect(process.env.MONGO)
     })
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => { console.log("Server is running on port 3000!!") })
+
+
+app.post('/api/signup', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const newUser = new User(
+        {
+            username, email, password
+        }
+    )
+    await newUser.save();
+    res.json("New user created");
+})
